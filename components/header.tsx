7a768313dea687f4fec08ba5dd32dba20dce2a26
Emir <emirpolitog@gmail.com>
2026-04-17 14:42:11 -0600

    feat: implement responsive Header component with mobile overlay menu and theme controls

--- components/header.tsx
+++ components/header.tsx
@@ -176,7 +176,7 @@ export const HeroHeader = () => {
                 className="mt-auto pb-11.5 space-y-9"
               >
                 {/* Theme Controls - Centered Cycle Mode */}
-                <div className="flex items-center justify-center pt-27.5">
+                <div className="flex items-center justify-center pt-28">
                   <ThemeControls isMobile={true} />
                 </div>
 
